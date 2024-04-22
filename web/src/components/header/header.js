844aefe7f8d2545bd7c6290d25b488a255a0a4ca
yyhago <yhago.felipe.teles@gmail.com>
2024-04-21 23:43:11 -0300

    Add Coments

--- web/src/components/header/header.js
+++ web/src/components/header/header.js
@@ -4,9 +4,11 @@ import Logo from '../../assets/LogoSemFundo.png'
 export default function Header({ logoVersion }){
   return(
     <div className="col-12">
-      <header className="py-4 px-4 text-center">
+
+      <header className="py-4 px-4 text-center"> {/*Logo com alteração manual com parâmetros*/}
         <img src={logoVersion ? LogoFundo : Logo} alt="LogoMundoPet" className="img-fluid" />
       </header>
+
     </div>
   )
 }
--- web/src/components/pages/cadastro/index.js
+++ web/src/components/pages/cadastro/index.js
@@ -4,12 +4,14 @@ import Illustration from '../../../assets/illustration.png'
 const Cadastro = () => {
   return(
     <div className="container-fluid h-100 bg-primary">
-      <Header logoVersion/>
+      <Header logoVersion/> {/*Componente Header*/}
       <div className="row ContentBox">
-        <div className="col-6 text-right my-auto">
+
+        <div className="col-6 text-right my-auto"> {/*Illustation*/}
           <img src={Illustration} alt="Logo" className="img-fluid imgIllustration" />
         </div>
-        <div className="col-6">
+
+        <div className="col-6"> {/*Formulário*/}
           <div className="box col-7">
             <h2 className="text-center">Falta bem pouco para mudar o humor do seu pet, e deixar ele feliz!</h2>
             <hr/>
@@ -21,11 +23,14 @@ const Cadastro = () => {
             <input type="date" className="form-control form-control-lg mb-3"/>
             <button className="btn btn-lg btn-block btn-secondary w-100">Finalizar Pedido</button>
           </div>
+
         </div>
       </div>
-      <div className="footerCadastro">
+
+      <div className="footerCadastro"> {/*Footer Cadastro*/}
         <strong>Não vai deixar seu pet na mão né!</strong>
       </div>
+
     </div>
   )
 }
