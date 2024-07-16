6dca832a599e8651b10aed4b5110eb1de1ad7580
yyhago <yhago.felipe.teles@gmail.com>
2024-07-16 13:43:12 -0300

    Screen Cadastro Responsive Finished!

--- web/src/components/pages/cadastro/index.js
+++ web/src/components/pages/cadastro/index.js
@@ -1,38 +1,41 @@
-import Header from '../../header/header'
-import Illustration from '../../../assets/illustration.png'
+import React from 'react';
+import Header from '../../header/header';
+import Illustration from '../../../assets/illustration.png';
 
 const Cadastro = () => {
-  return(
-    <div className="container-fluid h-100 bg-primary">
-      <Header logoVersion/> {/*Componente Header*/}
-      <div className="row ContentBox">
+  return (
+    <div className="d-flex flex-column h-100">
+      <div className="container-fluid flex-grow-1 bg-primary d-flex flex-column">
+        <Header logoVersion /> {/*Componente Header*/}
+        <div className="row flex-grow-1 ContentBox justify-content-center">
 
-        <div className="col-6 text-right my-auto"> {/*Illustation*/}
-          <img src={Illustration} alt="Logo" className="img-fluid imgIllustration" />
+        <div className="col-lg-6 d-none d-lg-flex text-center my-auto"> {/*Illustation*/}
+          <div className="glass-effect">
+            <img src={Illustration} alt="Logo" className="img-fluid imgIllustration" />
+          </div>
         </div>
 
-        <div className="col-6"> {/*Formulário*/}
-          <div className="box col-7">
-            <h2 className="text-center">Falta bem pouco para mudar o humor do seu pet, e deixar ele feliz!</h2>
-            <hr/>
-            <br/>
-            <input type="text" className="form-control form-control-lg mb-3" placeholder="Nome Completo" />
-            <input type="email" className="form-control form-control-lg mb-3" placeholder="E-mail" />
-            <input type="text" className="form-control form-control-lg mb-3" placeholder="Telefone" />
-            <input type="text" className="form-control form-control-lg mb-3" placeholder="CPF" />
-            <input type="date" className="form-control form-control-lg mb-3"/>
-            <button className="btn btn-lg btn-block btn-secondary w-100">Finalizar Pedido</button>
+          <div className="col-lg-6 col-md-8 col-12 d-flex justify-content-center align-items-center"> {/*Formulário*/}
+            <div className="box col-12 col-md-10 col-lg-8">
+              <h2 className="text-center">Falta bem pouco para mudar o humor do seu pet, e deixar ele feliz!</h2>
+              <hr />
+              <br />
+              <input type="text" className="form-control form-control-lg mb-3" placeholder="Nome Completo" />
+              <input type="email" className="form-control form-control-lg mb-3" placeholder="E-mail" />
+              <input type="text" className="form-control form-control-lg mb-3" placeholder="Telefone" />
+              <input type="text" className="form-control form-control-lg mb-3" placeholder="CPF" />
+              <input type="date" className="form-control form-control-lg mb-3" />
+              <button className="btn btn-lg btn-block btn-secondary w-100">Finalizar Pedido</button>
+            </div>
           </div>
-
         </div>
       </div>
 
       <div className="footerCadastro"> {/*Footer Cadastro*/}
         <strong>Não vai deixar seu pet na mão né!</strong>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Cadastro
+export default Cadastro;
--- web/src/index.js
+++ web/src/index.js
@@ -8,7 +8,7 @@ import './styles/global.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Checkout />
+    <Cadastro />
   </React.StrictMode>
 );
 
