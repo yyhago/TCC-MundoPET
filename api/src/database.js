9aa55aec2c3f5dc0a2e08408ad57ec31f422f710
yyhago <yhago.felipe.teles@gmail.com>
2024-11-02 21:43:29 -0300

    Connected mongoDB Atlas with service AWS

--- api/src/database.js
+++ api/src/database.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
-const URI = 'mongodb://localhost/mundopet';
+const URI = 'mongodb+srv://yyhago:Tele9815@mundopettcc.bxuhl.mongodb.net/?retryWrites=true&w=majority&appName=mundopetTCC';
 
 mongoose.set('debug', true);
 
 const connectDB = async () => {
   try {
     await mongoose.connect(URI);
-    console.log('Conectado ao DB - localhost');
+    console.log('Conectado ao DB - MongoDB Atlas');
   } catch (err) {
     console.error(err);
   }
