6492a6ae717de6eab7e2721747fe95286739cb9b
yyhago <yhago.felipe.teles@gmail.com>
2024-11-04 11:10:49 -0300

    Atualization data for pagaments and recebiments for useState with sucess!

--- web/src/components/sidebar/index.js
+++ web/src/components/sidebar/index.js
@@ -1,60 +1,55 @@
-import {useSelector} from 'react-redux'
-import { useState,useEffect } from 'react'
-import {Dock} from 'react-dock'
+import { useSelector } from 'react-redux';
+import { useState, useEffect } from 'react';
+import { Dock } from 'react-dock';
+import { useNavigate } from 'react-router-dom';
+import Product from '../../components/product/list';
 
-import Product from '../../components/product/list'
-
-
-import './styles.css'
+import './styles.css';
 
 const Sidebar = () => {
-
+  const navigate = useNavigate();
 
   const { cart } = useSelector(state => state.shop);
   const [opened, setOpened] = useState(false); 
 
   const total = cart.reduce((total, product) => {
     return total + product.preco;
-  }, 0)
+  }, 0);
 
-  useEffect(() => { // useEffect para esperar meu Event 'openCart' e alterar para true
+  useEffect(() => {
     window.addEventListener('openCart', () => {
-      // console.log('opencart!')
-      setOpened(true)
-    })
-  },[])
+      setOpened(true);
+    });
+  }, []);
 
   return (
-    <Dock // Chamada do DOCK, passando as propriedades de acordo com a documentação
+    <Dock 
       isVisible={opened} 
       onVisibleChange={(visible) => {
         setOpened(visible);
       }}
       position="right"
     >
-
-      {/*Conteúdo da minha SIDEBAR*/}
       <div className="container-fluid h-100 pt-4 sidebar">
         <h5>Minha Sacola ({cart.length})</h5> 
-
-        
         <div className="row products">
           {cart.map(p => <Product product={p} key={p._id} />)}
         </div>
-
-        <div className="row align-items-end footer"> {/*Footer da minha SIDEBAR*/}
+        <div className="row align-items-end footer">
           <div className="col-12 d-flex justify-content-between align-items-center">
             <b className='d-inline-block'>Total:</b>
             <h3 className='d-inline-block'>R$ {total.toFixed(2)}</h3>
           </div>
-          <button className='btn btn-block btn-lg btn-primary rounded-0 h-50 align-items-center'>Finalizar Compra!</button>
+          <button 
+            onClick={() => navigate('/checkout')}
+            className='btn btn-block btn-lg btn-primary rounded-0 h-50 align-items-center'
+          >
+            Finalizar Compra!
+          </button>
         </div>
-
       </div>
-
     </Dock>
-    
   );
-}
+};
 
 export default Sidebar;
--- web/src/pages/checkout/index.js
+++ web/src/pages/checkout/index.js
@@ -1,11 +1,77 @@
-import Header from '../../components/header/header'
-import Product from '../../components/product/list'
-import './styles.css'
+import dayjs from 'dayjs';
+import { useSelector } from 'react-redux';
+import { useState, useEffect } from 'react';
+
+import Header from '../../components/header/header';
+import Product from '../../components/product/list';
+import './styles.css';
 
 const Checkout = () => {
+  const { cart } = useSelector(state => state.shop);
+
+  const [transaction, setTransaction] = useState({
+    amount: 0,
+    card_number: '',
+    card_cvv: '',
+    card_expiration_date: '',
+    card_holder_name: '',
+    shipping: {
+      name: 'MundoPet LTDA',
+      fee: '1000',
+      delivery_date: dayjs().add(7, 'days').format('YYYY-MM-DD'),
+      expedited: true,
+      address: {
+        country: 'br',
+        state: '',
+        city: '',
+        neighborhood: '',
+        street: '',
+        street_number: '',
+        zipcode: '',
+      },
+    },
+    items: [],
+    split_rules: [],
+  });
+
+  const setShippingValue = (key, value) => {
+    setTransaction(prevTransaction => ({
+      ...prevTransaction,
+      shipping: {
+        ...prevTransaction.shipping,
+        address: {
+          ...prevTransaction.shipping.address,
+          [key]: value,
+        },
+      },
+    }));
+  };
+
+  const makePurchase = () => {
+    console.log(transaction);
+  };
+
+  const total = cart.reduce((total, product) => {
+    return total + parseFloat(product.preco); 
+  }, 0);
+
+  useEffect(() => {
+    setTransaction(prevTransaction => ({
+      ...prevTransaction,
+      amount: total.toFixed(2).toString().replace('.', ''),
+      items: cart.map(product => ({
+        id: product._id,
+        title: product.nome,
+        unit_price: parseFloat(product.preco).toFixed(2).toString().replace('.', ''),
+        quantity: 1,
+        tangible: true,
+      })),
+    }));
+  }, [cart]);
+
   return (
     <div className="h-100">
-      <Header hideCart/>
+      <Header hideCart />
 
       <div className="container mt-4">
         <div className="row">
@@ -13,73 +79,127 @@ const Checkout = () => {
             <span className='section-title'>Dados de Entrega</span>
             <div className="row mb-3">
               <div className="col-12">
-                <input type='text' placeholder='CEP' className='form-control form-control-lg' />
+                <input
+                  onChange={(e) => setShippingValue('zipcode', e.target.value)}
+                  type='text'
+                  placeholder='CEP'
+                  className='form-control form-control-lg'
+                />
               </div>
             </div>
             <div className="row mb-3">
               <div className="col-6">
-                <input type='text' placeholder='Cidade' className='form-control form-control-lg' />
+                <input
+                  onChange={(e) => setShippingValue('city', e.target.value)}
+                  type='text'
+                  placeholder='Cidade'
+                  className='form-control form-control-lg'
+                />
               </div>
               <div className="col-6">
-                <input type='text' placeholder='Logradouro' className='form-control form-control-lg' />
+                <input
+                  onChange={(e) => setShippingValue('street', e.target.value)}
+                  type='text'
+                  placeholder='Logradouro'
+                  className='form-control form-control-lg'
+                />
               </div>
             </div>
             <div className="row mb-3">
               <div className="col-5">
-                  <input type='text' placeholder='Bairro' className='form-control form-control-lg' />
-                </div>
-                <div className="col-5">
-                  <input type='text' placeholder='Número' className='form-control form-control-lg' />
-                </div>
-                <div className="col-2">
-                  <input type='text' placeholder='UF' className='form-control form-control-lg' />
-                </div>
+                <input
+                  onChange={(e) => setShippingValue('neighborhood', e.target.value)}
+                  type='text'
+                  placeholder='Bairro'
+                  className='form-control form-control-lg'
+                />
               </div>
-              <div className="col-12 mb-5">
-                  <input type='text' placeholder='Complemento' className='form-control form-control-lg' />
+              <div className="col-5">
+                <input
+                  onChange={(e) => setShippingValue('street_number', e.target.value)}
+                  type='text'
+                  placeholder='Número'
+                  className='form-control form-control-lg'
+                />
               </div>
-
-              <span className='section-title'>Dados de Pagamento</span>
-              <div className="row mb-3">
-                <div className="col-12 mb-3">
-                  <input type='text' placeholder='Nº do Cartão' className='form-control form-control-lg' />
-                </div>
-                <div className="col-6">
-                  <input type='text' placeholder='Validade do Cartão' className='form-control form-control-lg' />
-                </div>
-                <div className="col-6">
-                  <input type='text' placeholder='CVV' className='form-control form-control-lg' />
-                </div>
+              <div className="col-2">
+                <input
+                  onChange={(e) => setShippingValue('state', e.target.value)}
+                  type='text'
+                  placeholder='UF'
+                  className='form-control form-control-lg'
+                />
               </div>
-
-              <div className="row mt-4">
-                <div className="col-12 d-flex justify-content-between align-items-center">
-                  <b>Total</b>
-                  <h3>R$ 30,00</h3>
-                </div>
-                <div className="col-12">
-                  <button className="btn btn-lg btn-primary w-100">Finalizar Compra!</button>
-                </div>
-              </div> 
-
             </div>
-          
-          <div className="col-6">
-            <div className="box col mb-4 box-sidebar">
-              <h5>Minha Sacola!</h5>
+            <div className="col-12 mb-5">
+              <input
+                onChange={(e) => setShippingValue('complement', e.target.value)}
+                type='text'
+                placeholder='Complemento'
+                className='form-control form-control-lg'
+              />
+            </div>
 
-              <div className="row products">
-                {[1,2,3,4,5,6,7,8,9].map(p => <Product />)}
+            <span className='section-title'>Dados de Pagamento</span>
+            <div className="row mb-3">
+              <div className="col-6 mb-3">
+                <input
+                  onChange={(e) => setTransaction(prevTransaction => ({ ...prevTransaction, card_number: e.target.value }))}
+                  type='text'
+                  placeholder='Nº do Cartão'
+                  className='form-control form-control-lg'
+                />
               </div>
+              <div className="col-6 mb-3">
+                <input
+                  onChange={(e) => setTransaction(prevTransaction => ({ ...prevTransaction, card_holder_name: e.target.value }))}
+                  type='text'
+                  placeholder='Nome Completo no Cartão'
+                  className='form-control form-control-lg'
+                />
+              </div>
+              <div className="col-6">
+                <input
+                  onChange={(e) => setTransaction(prevTransaction => ({ ...prevTransaction, card_expiration_date: e.target.value }))}
+                  type='date'
+                  placeholder='dd/mm/aaaa'
+                  className='form-control form-control-lg'
+                />
+              </div>
+              <div className="col-6">
+                <input
+                  onChange={(e) => setTransaction(prevTransaction => ({ ...prevTransaction, card_cvv: e.target.value }))}
+                  type='text'
+                  placeholder='CVV'
+                  className='form-control form-control-lg'
+                />
+              </div>
+            </div>
 
+            <div className="row mt-4">
+              <div className="col-12 d-flex justify-content-between align-items-center">
+                <b>Total</b>
+                <h3>R$ {total.toFixed(2)}</h3>
+              </div>
+              <div className="col-12">
+                <button onClick={makePurchase} className="btn btn-lg btn-primary w-100">Finalizar Compra!</button>
+              </div>
             </div>
           </div>
-        
+
+          <div className="col-6">
+            <div className="box col mb-4 box-sidebar">
+              <h5>Minha Sacola ({cart.length})</h5>
+
+              <div className="row products">
+                {cart.map(p => <Product product={p} key={p._id} />)}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
 export default Checkout;
