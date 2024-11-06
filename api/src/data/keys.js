aefeec62c1cc76edb284b16735702929ed865be5
yyhago <yhago.felipe.teles@gmail.com>
2024-11-05 22:13:23 -0300

    Update key Paypal

--- api/src/data/keys.js
+++ api/src/data/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
     clientId: "AQU4eNOV2fulfGI698vq0XD2pEt4mdqJ4PhrDhN3qFj0-TseMBYk22NFn_6pzNSraNox8uAT7DQWM-0S",
-    secret: "ED0V9y30KDXAmXDI3kBvDGYWZHOgHXYMJvMOcqJHqiywY1SjyuqDHG8kynS6NeFX-XhRbuqC3MaaQ1zT"
+    secret: "EAn2MdwsYGfGNoNR6oZGlcxntWxIRU94k5TJ7WADeSbO2QsaDMhfouNWwfvszmWtp7N8WLQoWunak2fW"
 };
